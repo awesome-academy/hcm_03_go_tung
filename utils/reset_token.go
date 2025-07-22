2f524eb440e5c67568d0f045d578ca6ef9d56917
Tung Tran <tranthanhtung1212002@gmail.com>
2025-07-22 12:15:00 +0700

    WIP: reset password feature - initial commit

---
+++ utils/reset_token.go
@@ -0,0 +1,40 @@
+package utils
+
+import (
+    "github.com/golang-jwt/jwt/v5"
+    "time"
+)
+
+var resetSecret = []byte("super_secret_reset_key")
+
+// ResetClaim định nghĩa payload JWT khi reset password
+type ResetClaim struct {
+    UserID uint // ID của người dùng cần reset password
+    jwt.RegisteredClaims
+}
+
+// Tạo token reset có hạn trong 30 phút
+func GenerateResetToken(userID uint) (string, error) {
+    claims := ResetClaim{
+        UserID: userID,
+        RegisteredClaims: jwt.RegisteredClaims{
+            ExpiresAt: jwt.NewNumericDate(time.Now().Add(30 * time.Minute)),
+        },
+    }
+
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    return token.SignedString(resetSecret)
+}
+
+// Parse và xác thực token reset
+func ParseResetToken(tokenStr string) (*ResetClaim, error) {
+    token, err := jwt.ParseWithClaims(tokenStr, &ResetClaim{}, func(t *jwt.Token) (interface{}, error) {
+        return resetSecret, nil
+    })
+
+    if claims, ok := token.Claims.(*ResetClaim); ok && token.Valid {
+        return claims, nil
+    }
+
+    return nil, err
+}
